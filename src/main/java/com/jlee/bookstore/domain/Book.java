3b725d3fd54cf6f726468db3abb3fe843477bf14
joaoarthurcl <joaoarthurcl@gmail.com>
2021-06-08 23:56:10 +0100

    finished domain classes

--- src/main/java/com/jlee/bookstore/domain/Book.java
+++ src/main/java/com/jlee/bookstore/domain/Book.java
@@ -5,21 +5,30 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Book {
+public class Book implements Serializable {
+    public static final long serialVersionUID = 5024048380744116098L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String title;
     private String author_name;
     private String description;
     private String text;
 
+    @ManyToOne
+    @JoinColumn(name = "category_id")
     private Category category;
 
 }
--- src/main/java/com/jlee/bookstore/domain/Category.java
+++ src/main/java/com/jlee/bookstore/domain/Category.java
@@ -5,8 +5,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,13 +17,15 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Category {
+public class Category implements Serializable {
+    public static final long serialVersionUID = 5024048380744116098L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String description;
 
-    @OneToMany
+    @OneToMany(mappedBy = "category")
     private List<Book> books = new ArrayList<>();
 }
