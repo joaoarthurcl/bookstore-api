2a003cb5839610e850818bd10fdd7d4939869f97
joaoarthurcl <joaoarthurcl@gmail.com>
2021-06-09 00:02:28 +0100

    added repositories

---
+++ src/main/java/com/jlee/bookstore/repositories/BookRepository.java
@@ -0,0 +1,10 @@
+package com.jlee.bookstore.repositories;
+
+import com.jlee.bookstore.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+    
+}
