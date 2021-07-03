753c72d4498e259273698857db4d2d7bebc3a5f1
joaoarthurcl <joaoarthurcl@gmail.com>
2021-07-04 00:11:10 +0100

    Added more att in BookDTO

--- src/main/java/com/jlee/bookstore/dto/BookDTO.java
+++ src/main/java/com/jlee/bookstore/dto/BookDTO.java
@@ -19,20 +19,33 @@ public class BookDTO implements Serializable {
     public static final long serialVersionUID = 8857942005721366061L;
 
     private Integer id;
+
     @NotNull(message = "Field title is required!")
     @Length(min = 3, max = 50, message = "Min = 3 and Max = 50")
     private String title;
+
+    @NotNull(message = "Field author_name is required!")
+    @Length(min = 3, max = 50, message = "Min = 3 and Max = 50")
+    private String author_name;
+
+    @NotNull(message = "Field description is required!")
+    @Length(min = 3, max = 2000000, message = "Min = 3 and Max = 2000000")
+    private String description;
 
     public BookDTO(com.jlee.bookstore.domain.Book book) {
         super();
         this.id = book.getId();
         this.title = book.getTitle();
+        this.author_name = book.getAuthor_name();
+        this.description = book.getDescription();
     }
 
     public static BookDTO ofEntity(com.jlee.bookstore.domain.Book book) {
         return BookDTO.builder()
                 .id(book.getId())
                 .title(book.getTitle())
+                .author_name(book.getAuthor_name())
+                .description(book.getDescription())
                 .build();
     }
 }
